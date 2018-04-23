6f82aad37349ccfcc866a6e547098f2a0f054303
NormaalBart <bartkempen10@gmail.com>
2018-04-23 20:11:32 +0200

    First commit

---
+++ src/main/java/me/bartvv/teams/Teams.java
@@ -0,0 +1,13 @@
+package me.bartvv.teams;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Teams extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+
+}
